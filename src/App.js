8eb1178f2e24ba89bcad254676c42e73b281bb57
ValeSinSal <ocampogabriela424@gmail.com>
2023-06-30 20:51:04 -0500

    1

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class="e19_4">
+        <div  class="e32_60"></div>
+        <div  class="e21_54"></div>
+        <div  class="e21_52"></div>
+        <div  class="e19_6"></div>
+        <span  class="e19_5">WEBCAT - Que tipo de Gato eres</span>
+        <div  class="e19_7"></div>
+        <div  class="e19_9"></div>
+        <div  class="e19_8"></div>
+        <div  class="e20_10"></div>
+        <div  class="e20_13"></div>
+        <div  class="e21_15"></div>
+        <div  class="e21_16"></div>
+        <div  class="e20_14"></div>
+        <span  class="e21_17">Gato Negro</span>
+        <span  class="e21_18">Los gatos negros suelen ser personalidades tranquilas y calmadas, suelen ser afectuosos únicamente con sus dueños, son animales asociales y tranquilos</span>
+        <span  class="e21_32">Gato blanco</span>
+        <span  class="e21_33">Son gatos asociales incluso con sus dueños, extremadamente perezosos pues duermen todo el día, son poco activos y suelen ser glotones, es el tipo de gato que si lo molestas te ignorará, sus únicas actividades son comer y dormir; muy pocas veces buscan por el afecto de sus dueños</span>
+        <span  class="e21_30">Gato gris</span>
+        <span  class="e21_31">Estos gatos suelen ser desconfiados al principio, parecerán gatos distantes pero cuando agarran confianza la historia cambia, son exageradamente energéticos y adoran jugar pero también son algo gruñones si los molestas mucho y puede que te aruñen si los haces enojar</span>
+        <span  class="e21_27">Gato naranja</span>
+        <span  class="e21_28">Son gatos con una personalidad extrovertida y sociable, le gusta el contacto humano y siempre estarán activos porque son muy inquietos y juguetones, a diferencia de los otros gatos esos adoran a la gente y corretear por allí</span>
+        <button class="custom-btn1 btn-1">Conoce más Acerca de Esto</button>
+        <button class="custom-btn2 btn-2">Conoce más Acerca de Esto</button>
+        <button class="custom-btn3 btn-3">Conoce más Acerca de Esto</button>
+        <button class="custom-btn4 btn-4">Conoce más Acerca de Esto</button>
+        <div  class="e21_41"></div>
+        <div  class="e21_26"></div>
+        <div  class="e32_63"></div>
+        <div  class="e32_61"></div>
+        <div  class="e32_62"></div>
+        <span  class="e31_55">Naranja</span>
+        <span  class="e31_56">Negro</span>
+        <span  class="e31_57">Blanco</span>
+        <span  class="e31_58">--Gris--</span>
+        <span  class="e32_64">Tu personalidad única puede reflejar algunas características felinas 
+fascinantes. Imagina ser un gato y explorar diferentes aspectos 
+de tu personalidad a través de la lente de estos maravillosos animales. 
+Los gatos son conocidos por su elegancia, independencia y misterio. 
+Algunos son aventureros y curiosos, mientras que otros prefieren 
+la tranquilidad y la comodidad de su hogar.</span></div>
+          </body>
+  );
+}
+
+export default App;
